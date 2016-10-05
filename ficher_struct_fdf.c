7c3f031f8861ab8d85530a57d49ed1fbdd59dde3
Salomon MEJIA MEDINA <smejia-m@e2r7p21.42.fr>
2016-10-05 15:35:24 +0200

    parseur fini

--- ficher_struct_fdf.c
+++ ficher_struct_fdf.c
@@ -17,7 +17,6 @@ int ft_cheknum(char *map)
 			//ft_putchar('b');//
 			if (map[i] == ' ' || map[i] == '-' || map[i] == '\n')
 			{
-
 				i++;
 			//ft_putchar('a');//
 			}
@@ -29,12 +28,50 @@ int ft_cheknum(char *map)
 		}
 		else
 		{
-
 		//ft_putchar('c');//
 		i++;
 		}
 	}
 	return (0);
+}
+
+/*
+** fonction qui verifie que la longueur de lignes soit la même
+*/
+
+int ft_checkline(t_param *params)
+{
+	printf("tmp len : %d\n", params->tmp_len);
+	printf("len : %d\n", params->len);
+	if(params->tmp_len != 0)
+	{
+		if (params->tmp_len != params->len)
+			return (-1);
+		else
+			return (0);
+	}
+	return (0);
+}
+
+/*
+**fonction qui mesure la longuer du tableau de chaine de caracteres
+*/
+
+static	int		count(char const *s, char c)
+{
+	size_t		i;
+	int			len;
+
+	i = 1;
+	len = 0;
+	while (s[i] != '\0')
+	{
+		if ((s[i - 1] != c && s[i] == c) || (i == 0 && s[i] != c)
+			|| (i == ft_strlen(s) - 1 && s[i] != c))
+			len++;
+		i++;
+	}
+	return (len);
 }
 
 /*
@@ -60,6 +97,8 @@ t_param *ft_param_ini(void)
 	params->index_y = 0;
 	params->index_x = 0;
 	params->count_lines = 0;
+	params->tmp_len = 0;
+	params->len = 0;
 	return (params);
 }
 
@@ -106,15 +145,18 @@ int ft_reader(int fd, char *line)
 	while ((ret = get_next_line(fd, &line) > 0))
 	{
 		params->index_y++;
-		if(ft_cheknum(line) == -1)
+		if(ft_cheknum(line) == -1 || ft_checkline(params) == -1) 
 			return (-1);
 		printf("ret : %d line : %s\n ",ret , line); //
 		params->tab = ft_strsplit(line, ' ');
+		params->tmp_len = params->len;
+		params->len = count(line, ' ');
 		coor = ft_toplace(params->tab, params->index_y);
 		new_lst = ft_lstnew(coor, sizeof(coor));
 		ft_lstadd(&lst, new_lst);
         free(line);
     }
+    free(coor);
     return(0);
 }
 
@@ -142,6 +184,7 @@ int main (int ac, char **av)
 		//ft_putstr("test");//
 		fd = open(av[1], O_RDONLY);
 		ret = ft_reader(fd, line);
+		ft_putstr("wiki wiki");//
 	}
 	else
 	{ 
